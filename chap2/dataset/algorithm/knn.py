0ab6a6b90553265ae8d2164cd68dbb5b41a5dc73
socurites <socurites@wise-fashion.com>
2019-04-29 19:57:11 +0900

    Add knn classifier example

--- chap2/dataset/algorithm/knn.py
+++ chap2/dataset/algorithm/knn.py
@@ -29,4 +29,42 @@ print("Prediction: {}".format(classifier.predict(X_test)))
 # Evaluate
 print("Accuracy: {:.2f}".format(classifier.score(X_test, y_test)))
 
-# Decision boundary
+# Decision boundary with forge dataset
+fig, axes = plt.subplots(1, 3, figsize=(10, 3))
+
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    classifier = KNeighborsClassifier(n_neighbors=n_neighbors).fit(X, y)
+
+    mglearn.plots.plot_2d_separator(classifier, X, fill=True, eps=0.5, ax=ax, alpha=.4)
+    mglearn.discrete_scatter(X[:, 0], X[:, 1], y, ax=ax)
+    ax.set_title("{} neighbor".format(n_neighbors))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+
+axes[0].legend(loc=3)
+plt.show()
+
+
+# Complexity/Generalization of Model with Cancer dataset
+from sklearn.datasets import load_breast_cancer
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=66)
+
+training_accuracy = []
+test_accuracy = []
+
+neighbors_settings = range(1, 11)
+for n_neighbors in neighbors_settings:
+    classifier = KNeighborsClassifier(n_neighbors=n_neighbors)
+    classifier.fit(X_train, y_train)
+
+    training_accuracy.append(classifier.score(X_train, y_train))
+    test_accuracy.append(classifier.score(X_test, y_test))
+
+plt.plot(neighbors_settings, training_accuracy, label='training accuracy')
+plt.plot(neighbors_settings, test_accuracy, label='test accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('n_neighbors')
+plt.legend()
+plt.show()
