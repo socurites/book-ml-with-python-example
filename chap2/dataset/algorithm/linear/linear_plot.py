9dbedc2deb13182ece29f08302ae92f3da5a97b3
socurites <socurites@wise-fashion.com>
2019-04-29 20:57:40 +0900

    Add linear regression example

---
+++ chap2/dataset/algorithm/linear/linear_plot.py
@@ -0,0 +1,7 @@
+import mglearn
+import matplotlib.pyplot as plt
+
+"""Plot
+"""
+mglearn.plots.plot_linear_regression_wave()
+plt.show()
---
+++ chap2/dataset/algorithm/linear/linear_regression.py
@@ -0,0 +1,27 @@
+"""Linear Regression
+OLS: Ordinary Least Squares
+"""
+
+import mglearn
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+"""LinearRegressiopn with wave dataset
+"""
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+regression = regressor.fit(X_train, y_train)
+
+print("coef_: {}".format(regression.coef_))
+print("intercept_: {}".format(regression.intercept_))
+
+print("Train R^2: {:.2f}".format(regression.score(X_train, y_train)))
+print("Test R^2: {:.2f}".format(regression.score(X_test, y_test)))
+
+
+"""LinearRegression with Boston dataset
+"""
