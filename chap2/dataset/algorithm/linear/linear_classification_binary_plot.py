ebdbfdc95a9203f111f833a13c95294d0b6c875b
socurites <socurites@wise-fashion.com>
2019-05-01 11:29:22 +0900

    Add linear classification plot

---
+++ chap2/dataset/algorithm/linear/linear_classification_binary_plot.py
@@ -0,0 +1,20 @@
+import mglearn
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+X, y = mglearn.datasets.make_forge()
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+
+for model, ax in zip([LinearSVC(), LogisticRegression()], axes):
+    classification = model.fit(X, y)
+    mglearn.plots.plot_2d_separator(classification, X, fill=False, eps=0.5, ax=ax, alpha=.7)
+    mglearn.discrete_scatter((X[:, 0]), X[:, 1], y, ax=ax)
+    ax.set_title("{}".format(classification.__class__.__name__))
+    ax.set_xlabel("Feature 0")
+    ax.set_ylabel("Feature 1")
+
+axes[0].legend()
+plt.show()
