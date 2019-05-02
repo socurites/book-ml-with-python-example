e7074892a9ea3bf212ee9486b38689cce3df1330
socurites <socurites@wise-fashion.com>
2019-05-02 19:21:42 +0900

    Add neural network example by cancer datasets

--- chap2/dataset/algorithm/neural_network/mlp_classification.py
+++ chap2/dataset/algorithm/neural_network/mlp_classification.py
@@ -30,3 +30,40 @@ plt.show()
 
 """MLP with cancer dataset
 """
+from sklearn.datasets import load_breast_cancer
+cancer = load_breast_cancer()
+
+X_train, X_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=0)
+
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train, y_train)
+
+print("Train accuracy: {:.3f}".format(mlp.score(X_train, y_train)))
+print("Test accuracy: {:.3f}".format(mlp.score(X_test, y_test)))
+
+# Standardizing datasets
+print("Max per features:\n{}".format(cancer.data.max(axis=0)))
+
+mean_on_train = X_train.mean(axis=0)
+std_on_train = X_train.std(axis=0)
+
+X_train_scaled = (X_train - mean_on_train) / std_on_train
+X_test_scaled = (X_test - mean_on_train) / std_on_train
+
+mlp = MLPClassifier(random_state=42)
+mlp.fit(X_train_scaled, y_train)
+
+print("Train accuracy: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
+print("Test accuracy: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+mlp = MLPClassifier(max_iter=1000, random_state=42)
+mlp.fit(X_train_scaled, y_train)
+
+print("Train accuracy: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
+print("Test accuracy: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
+
+mlp = MLPClassifier(max_iter=1000, alpha=1, random_state=42)
+mlp.fit(X_train_scaled, y_train)
+
+print("Train accuracy: {:.3f}".format(mlp.score(X_train_scaled, y_train)))
+print("Test accuracy: {:.3f}".format(mlp.score(X_test_scaled, y_test)))
